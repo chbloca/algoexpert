6e9e5d5d75eadfedc3a9c2faeb4ae7e7580ee5bd
chbloca <christen.bc@gmail.com>
2022-01-21 10:04:12 +0100

    add non-constructible change template

---
+++ nonconstructible_change/main.cpp
@@ -0,0 +1,27 @@
+/**
+ * A team receives 3 points if it wins and 0 if it loses.
+ * Sample Input
+ * coins = [5, 7, 1, 1, 2, 3, 22]
+ * results = 20
+ * 
+ * Sample Output
+ * "Python"
+ * C# beats HTML, Python beats C#, and Python beats HTML
+ */
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int nonConstructibleChange(vector<int> coins) {
+  // Write your code here.
+  return -1;
+}
+
+int main () {
+    int output = nonConstructibleChange({5, 7, 1, 1, 2, 3, 22});
+
+    cout << output << endl;
+}
