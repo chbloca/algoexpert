65591c7cc26acc07d835b988d07c11b0577c4217
chbloca <christen.bc@gmail.com>
2022-01-21 10:40:21 +0100

    implement non-constructible change

--- nonconstructible_change/main.cpp
+++ nonconstructible_change/main.cpp
@@ -1,27 +1,37 @@
 /**
- * A team receives 3 points if it wins and 0 if it loses.
  * Sample Input
  * coins = [5, 7, 1, 1, 2, 3, 22]
- * results = 20
  * 
  * Sample Output
- * "Python"
- * C# beats HTML, Python beats C#, and Python beats HTML
+ * results = 20
  */
 
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
 int nonConstructibleChange(vector<int> coins) {
-  // Write your code here.
-  return -1;
+    // Write your code here.
+    // O(nlogn [sort] + n [loop]) --> O(nlogn) time
+    // O(1) [variable is manipulated and no auxiliar variable is created] space
+    sort(coins.begin(), coins.end());
+
+    int currentChangeCreated = 0;
+    for (auto coin : coins) {
+        if (coin > currentChangeCreated + 1)
+            return currentChangeCreated + 1;
+
+        currentChangeCreated += coin; 
+    }
+    return currentChangeCreated + 1; // this is for the case no coins are given
 }
 
 int main () {
     int output = nonConstructibleChange({5, 7, 1, 1, 2, 3, 22});
+    int output2 = nonConstructibleChange({});
 
     cout << output << endl;
+    cout << output2 << endl;
 }
