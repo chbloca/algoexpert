731ca607e5f2af4774fe2e55fde43e00db0caaf6
chbloca <christen.bc@gmail.com>
2022-01-25 21:00:45 +0100

    implement node depths

--- node_depths/main.cpp
+++ node_depths/main.cpp
@@ -9,16 +9,17 @@
  * 8    9 10      
  * 
  * Sample Output
- * 16
+ * 19
  * The depth of the node with value 2 is 1.
  * The depth of the node with value 3 is 1.
  * The depth of the node with value 4 is 2.
  * The depth of the node with value 5 is 2.
  * ...
- * Summing all of these depths yields 16
+ * Summing all of these depths yields 19
  */
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -35,9 +36,30 @@ public:
   }
 };
 
+struct Level {
+    BinaryTree *root;
+    int depth;
+};
+
 int nodeDepths(BinaryTree *root) {
   // Write your code here.
-  return -1;
+  // O(n) time whe n is the number of nodes
+  // O(h) space is the height of the tree
+  int sumOfDepths = 0;
+  vector<Level> stack = {{root, 0}};
+
+  while (stack.size() > 0) {
+      BinaryTree *node = stack.back().root;
+      int depth = stack.back().depth;
+      stack.pop_back();
+      if (node == nullptr)
+        continue;
+    sumOfDepths += depth;
+    stack.push_back(Level{node->left, depth + 1});
+    stack.push_back(Level{node->right, depth + 1});
+  }
+
+  return sumOfDepths;
 }
 
 int main () {
