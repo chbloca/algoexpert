12337d106cb3337d5a347993ba04dcfcb9aa5eb5
chbloca <christen.bc@gmail.com>
2022-01-27 19:42:58 +0100

    add class photos template

---
+++ class_photos/main.cpp
@@ -0,0 +1,23 @@
+/**
+ * Sample Input
+ * redShirtHeights = [5, 8, 1, 3, 4]    
+ * blueShirtHeights = [6, 9, 2, 4, 5]    
+ * 
+ * Sample Output
+ * true
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool classPhotos(vector<int> redShirtHeights, vector<int> blueShirtHeights) {
+  // Write your code here.
+  return false;
+}
+
+int main () {
+    int result = classPhotos({5, 8, 1, 3, 4}, {6, 9, 2, 4, 5});
+
+    cout << result << endl;
+}
