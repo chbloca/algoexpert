d33b383ea9aa90e290c74c4dffeb320e1df0395b
chbloca <christen.bc@gmail.com>
2022-01-27 21:49:41 +0100

    implement class photos

--- class_photos/main.cpp
+++ class_photos/main.cpp
@@ -9,11 +9,31 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 bool classPhotos(vector<int> redShirtHeights, vector<int> blueShirtHeights) {
   // Write your code here.
-  return false;
+  // O(nlog n) time
+  // O(1) space
+  sort(redShirtHeights.rbegin(), redShirtHeights.rend()); // in the reverse order
+  sort(blueShirtHeights.rbegin(), blueShirtHeights.rend());
+
+  string shirtColorInFirstRow = redShirtHeights[0] < blueShirtHeights[0] ? "RED" : "BLUE";
+
+  for (int i = 0; i < redShirtHeights.size(); i++) {
+      int redShirtHeight = redShirtHeights[i];
+      int blueShirtHeight = blueShirtHeights[i];
+
+      if (shirtColorInFirstRow == "RED") {
+          if (redShirtHeight >= blueShirtHeight)
+            return false;
+      } else {
+          if (blueShirtHeight >= redShirtHeight)
+            return false;
+      }
+  }
+  return true;
 }
 
 int main () {
