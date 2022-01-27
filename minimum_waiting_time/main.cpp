aa3d40728e98af04e3b7c5132ba0a2d7f32cdeec
chbloca <christen.bc@gmail.com>
2022-01-27 19:37:47 +0100

    implement minimum waiting time

--- minimum_waiting_time/main.cpp
+++ minimum_waiting_time/main.cpp
@@ -3,17 +3,31 @@
  * queries = [3, 2, 1, 2, 6]    
  * 
  * Sample Output
+ * [1, 2, 2, 3, 6]
+ * 
  * 17
  */
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int minimumWaitingTime(vector<int> queries) {
   // Write your code here.
-  return -1;
+  // O(nlog n [sort] + n [for]) => O(nlog n) time
+  // O(1) space
+  sort(queries.begin(), queries.end());
+
+  int waitingDuration = 0;
+  for (int i = 0; i < queries.size(); i++) {
+      int duration = queries[i];
+      int queriesLeft = queries.size() - (i + 1);
+      waitingDuration += duration * queriesLeft;
+  }
+
+  return waitingDuration;
 }
 
 int main () {
