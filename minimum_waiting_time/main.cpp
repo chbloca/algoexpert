02ff3ef449c916e74fc378f2253070c0a09f60c4
chbloca <christen.bc@gmail.com>
2022-01-27 19:27:19 +0100

    add minimum waiting time template

---
+++ minimum_waiting_time/main.cpp
@@ -0,0 +1,23 @@
+/**
+ * Sample Input
+ * queries = [3, 2, 1, 2, 6]    
+ * 
+ * Sample Output
+ * 17
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int minimumWaitingTime(vector<int> queries) {
+  // Write your code here.
+  return -1;
+}
+
+int main () {
+    int result = minimumWaitingTime({3, 2, 1, 2, 6});
+
+    cout << result << endl;
+}
