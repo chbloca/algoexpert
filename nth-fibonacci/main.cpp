edccadaf98add238293ca5fab5a01cebfa417473
chbloca <christen.bc@gmail.com>
2022-02-01 18:56:04 +0100

    add nth fibonacci template

---
+++ nth-fibonacci/main.cpp
@@ -0,0 +1,26 @@
+/**
+ * Sample Input
+ * n = 2
+ * 
+ * Sample Output
+ * 1 // 0, 1
+ * 5 // 0, 1, 1, 2, 3, 5
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int getNthFib(int n) {
+  // Write your code here.
+  return -1;
+}
+
+int main () {
+    int output = getNthFib(1);
+    int output2 = getNthFib(5);
+
+    cout << output << endl;
+    cout << output2 << endl;
+}
