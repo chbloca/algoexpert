c27d883f80d9e66c7bfa22ac04305487d17239ad
chbloca <christen.bc@gmail.com>
2022-02-02 18:05:26 +0100

    add binary search template

---
+++ binary_search/main.cpp
@@ -0,0 +1,24 @@
+/**
+ * Sample Input
+ * array = [0, 1, 21, 33, 45, 45, 61, 71, 72, 73]
+ * target = 33   
+ * 
+ * Sample Output
+ * 3
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int binarySearch(vector<int> array, int target) {
+  // Write your code here.
+  return -1;
+}
+
+int main () {
+    int result = binarySearch({0, 1, 21, 33, 45, 45, 61, 71, 72, 73}, 33);
+
+    cout << result << endl;
+}
