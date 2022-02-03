24740f5b0fc77fb0f3330acee56d3b997699058e
chbloca <christen.bc@gmail.com>
2022-02-03 20:04:23 +0100

    implement bubble sort

--- bubble_sort/main.cpp
+++ bubble_sort/main.cpp
@@ -12,8 +12,23 @@
 using namespace std;
 
 vector<int> bubbleSort(vector<int> array) {
-  // Write your code here.
-  return {};
+    // Write your code here.
+    // O(n^2) time for the worst / average case, since the array is traversed multiple times
+    // O(n) time for the best case, since the array is traversed once
+    // O(1) space
+    bool isSorted = false;
+    int counter = 0;
+    while (!isSorted) {
+        isSorted = true;
+        for (int i = 0; i < array.size() - 1 - counter; i++) {
+            if (array[i] > array[i + 1]) {
+                swap(array[i], array[i + 1]);
+                isSorted = false;
+            }
+        }
+        counter++;
+    }
+    return array;
 }
 
 int main () {
