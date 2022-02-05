930458e1fd7c4799b3949496d04f7d8e8e58c3c8
chbloca <christen.bc@gmail.com>
2022-02-05 20:14:05 +0100

    implement palindrome interatively

--- palindrome/main.cpp
+++ palindrome/main.cpp
@@ -11,12 +11,25 @@
 using namespace std;
 
 bool isPalindrome(string str) {
-  // Write your code here.
-  return false;
+    // Write your code here.
+    // O(n) time
+    // O(1) space
+    int leftIdx = 0;
+    int rightIdx = str.size() - 1;
+
+    while (leftIdx != rightIdx) {
+        if (str[leftIdx] != str[rightIdx])
+            return false;
+
+        leftIdx++;
+        rightIdx--;        
+    }
+
+    return true;
 }
 
 int main () {
-    bool result = isPalindrome("abcdcba");
+    bool result = isPalindrome("abcdeba");
 
     cout << result << endl;
 }
