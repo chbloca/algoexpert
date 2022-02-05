1e34a493c6d25e1735c6be906d8b56b9e6d3155e
chbloca <christen.bc@gmail.com>
2022-02-05 19:56:38 +0100

    add palindrome template

---
+++ palindrome/main.cpp
@@ -0,0 +1,22 @@
+/**
+ * Sample Input
+ * string = "abcdcba"
+ * 
+ * Sample Output
+ * true // it's written the same forward and backward
+ */
+
+#include <iostream>
+
+using namespace std;
+
+bool isPalindrome(string str) {
+  // Write your code here.
+  return false;
+}
+
+int main () {
+    bool result = isPalindrome("abcdcba");
+
+    cout << result << endl;
+}
