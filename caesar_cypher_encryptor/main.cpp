1e708f579e4ca0bb8dbb2652ba6d1ac8547a2277
chbloca <christen.bc@gmail.com>
2022-02-14 18:28:00 +0100

    add caesar cypher encryptor template

---
+++ caesar_cypher_encryptor/main.cpp
@@ -0,0 +1,24 @@
+/**
+ * Sample Input
+ * string = "xyz"
+ * key = 2
+ * 
+ * Sample Output
+ * "zab"
+ */
+
+#include <iostream>
+
+using namespace std;
+
+
+string caesarCypherEncryptor(string str, int key) {
+  // Write your code here.
+  return "";
+}
+
+int main () {
+    string result = caesarCypherEncryptor("xyz", 2);
+
+    cout << result << endl;
+}
