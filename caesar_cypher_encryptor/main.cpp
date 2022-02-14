b370695cdc77427f33bad02bce79a9f66a85a06f
chbloca <christen.bc@gmail.com>
2022-02-14 18:51:27 +0100

    implement caesar cypher encryptor

--- caesar_cypher_encryptor/main.cpp
+++ caesar_cypher_encryptor/main.cpp
@@ -8,13 +8,30 @@
  */
 
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
+char getLetterCode(char letter, int key) {
+    int newLetterCode = letter + key;
+    return newLetterCode <= 122 ? newLetterCode : 96 + newLetterCode % 122; // a = 97, z = 122
+}
 
 string caesarCypherEncryptor(string str, int key) {
-  // Write your code here.
-  return "";
+    // Write your code here.
+    // O(n) time
+    // O(n) space
+
+    vector<char> newLetters;
+
+    int newKey = key % 26; // 26 is the # of letters in the alphabet
+
+    for (auto &letter : str) {
+        newLetters.push_back(getLetterCode(letter, newKey));
+    }
+
+    return string(newLetters.begin(), newLetters.end());
 }
 
 int main () {
