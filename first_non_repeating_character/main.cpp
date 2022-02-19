f7d22f6195749906cc13ab0601954ba4e2803411
chbloca <christen.bc@gmail.com>
2022-02-19 17:04:39 +0100

    implement first non-repeating character

--- first_non_repeating_character/main.cpp
+++ first_non_repeating_character/main.cpp
@@ -7,12 +7,29 @@
  */
 
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
 int firstNonRepeatingCharacter(string string) {
-  // Write your code here.
-  return -1;
+    // Write your code here.
+    // O(2n) --> O(n) time
+    // O(26) --> O(1) space
+    unordered_map<char, int> characterFrequencies;
+    for (char &character : string) {
+        if (characterFrequencies.find(character) == characterFrequencies.end()) {
+            characterFrequencies[character] = 0;
+        }
+        characterFrequencies[character]++;
+    }
+    for (int idx = 0; idx < string.length(); idx++) {
+        char character = string[idx];
+        if (characterFrequencies[character] == 1) {
+            return idx;
+        }
+    }
+
+    return -1;
 }
 
 int main () { 
