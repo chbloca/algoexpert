b1767c0efdb2888b5d2d8aebc2ca5c6c0eddf62d
chbloca <christen.bc@gmail.com>
2022-02-19 16:43:26 +0100

    implement generate document

--- generate_document/main.cpp
+++ generate_document/main.cpp
@@ -8,16 +8,34 @@
  */
 
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
 bool generateDocument(string characters, string document) {
   // Write your code here.
-  return false;
+  // O(n + m) time
+  // O(c) space
+  unordered_map<char, int> characterCounts;
+
+  for (char &character : characters) {
+      if (characterCounts.find(character) == characterCounts.end()) {
+          characterCounts[character] = 0;
+      }
+      characterCounts[character]++;
+  }
+
+  for (char &character : document) {
+      if (characterCounts.find(character) == characterCounts.end() || characterCounts[character] == 0) {
+          return false;
+      }
+      characterCounts[character]--;
+  }
+  return true;
 }
 
 int main () { 
-    bool result = generateDocument("AAAAAAAAAAAAABBCCCCDD", "AlgoExpert is the Best!");
+    bool result = generateDocument("Bste!hetsi ogEAxpelrt x ", "AlgoExpert is the Best!");
 
     cout << result << endl;
 }
