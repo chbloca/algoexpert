0f0ae145c0d98eec4cf773468e8d3e1cad7ff37f
chbloca <christen.bc@gmail.com>
2022-02-19 15:56:31 +0100

    add generate document template

---
+++ generate_document/main.cpp
@@ -0,0 +1,23 @@
+/**
+ * Sample Input
+ * characters = "Bste!hetsi ogEAxpelrt x "
+ * document = "AlgoExpert is the Best!"
+ * 
+ * Sample Output
+ * true
+ */
+
+#include <iostream>
+
+using namespace std;
+
+bool generateDocument(string characters, string document) {
+  // Write your code here.
+  return false;
+}
+
+int main () { 
+    bool result = generateDocument("AAAAAAAAAAAAABBCCCCDD", "AlgoExpert is the Best!");
+
+    cout << result << endl;
+}
