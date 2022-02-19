0d549b355aa4c098c7881bed53b6fd2f9358d7a8
chbloca <christen.bc@gmail.com>
2022-02-19 16:46:50 +0100

    add first non repeating character template

---
+++ first_non_repeating_character/main.cpp
@@ -0,0 +1,22 @@
+/**
+ * Sample Input
+ * string = "abcdcaf"
+ * 
+ * Sample Output
+ * 1 // The first non-repeating character is "b" and is found at index 1
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int firstNonRepeatingCharacter(string string) {
+  // Write your code here.
+  return -1;
+}
+
+int main () { 
+    int result = firstNonRepeatingCharacter("abcdcaf");
+
+    cout << result << endl;
+}
