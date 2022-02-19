61b5725cbaa783fa83e07ee3abc347fbbbe09c00
chbloca <christen.bc@gmail.com>
2022-02-19 15:22:19 +0100

    add run length enconding template

---
+++ run_length_encoding/main.cpp
@@ -0,0 +1,22 @@
+/**
+ * Sample Input
+ * string = "AAAAAAAAAAAAABBCCCCDD"
+ * 
+ * Sample Output
+ * "9A4A2B4C2D"
+ */
+
+#include <iostream>
+
+using namespace std;
+
+string runLengthEncoding(string str) {
+  // Write your code here.
+  return "";
+}
+
+int main () { 
+    string result = runLengthEncoding("AAAAAAAAAAAAABBCCCCDD");
+
+    cout << result << endl;
+}
