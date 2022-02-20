337ba7815dfe5021d1a46fea9c10aee416980221
chbloca <christen.bc@gmail.com>
2022-02-20 11:25:53 +0100

    add power recursive implementation

--- extra/power/main.cpp
+++ extra/power/main.cpp
@@ -1,10 +1,11 @@
 // power() returns base raised to the power exp, ignoring over- and overflow
+// https://www.geeksforgeeks.org/write-a-c-program-to-calculate-powxn/
 
 #include <iostream>
 
 using namespace std;
 
-int power(int base, int exp) {
+int powerNaive(int base, int exp) {
     // Write your code here
     // O(n) time
     // O(1) space
@@ -16,8 +17,22 @@ int power(int base, int exp) {
     return result;
 }
 
+int power(int base, int exp) {
+    // O(log n) time
+    // O(1) space
+    if (exp == 0)
+        return 1;
+    
+    if (base == 0)
+        return 0;
+    
+    return base * power(base, exp - 1);
+}
+
 int main() {
+    int resultNaive = powerNaive(2, 4);
     int result = power(2, 4);
 
+    cout << resultNaive << endl;
     cout << result << endl;
 }
