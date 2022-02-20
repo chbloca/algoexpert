ca4c5192e0b17c4f96eec6572f7ebe1f584193a4
chbloca <christen.bc@gmail.com>
2022-02-20 10:46:08 +0100

    add power template

---
+++ extra/power/main.cpp
@@ -0,0 +1,16 @@
+// power() returns base raised to the power exp, ignoring over- and overflow
+
+#include <iostream>
+
+using namespace std;
+
+int power(int base, int exp) {
+    // Write your code here
+
+}
+
+int main() {
+    int result = power(2, 2);
+
+    cout << result << endl;
+}
