caf432140d5fa39f893137b2042c27eaf2507e9f
chbloca <christen.bc@gmail.com>
2022-02-21 22:45:34 +0100

    three number sum

---
+++ three_number_sum/main.cpp
@@ -0,0 +1,24 @@
+/**
+ * Sample Input
+ * array = [12, 3, 1, 2, -6, 5, -8, 6]
+ * targetSum = 0
+ * 
+ * Sample Output
+ * [[-8, 2, 6], [-8, 3, 5], [-6, 1, 5]]
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> threeNumberSum(vector<int> array, int targetSum) {
+  // Write your code here.
+  return {};
+}
+
+int main() {
+    vector<vector<int>> result = threeNumberSum({12, 3, 1, 2, -6, 5, -8, 6}, 0);
+
+    // cout << result << endl;
+}
