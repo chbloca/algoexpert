1cf6328900bb377f9af0dcf14e0af6a67abea0fe
chbloca <christen.bc@gmail.com>
2022-02-21 23:10:21 +0100

    add three number sum implementation

--- three_number_sum/main.cpp
+++ three_number_sum/main.cpp
@@ -9,16 +9,41 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector<vector<int>> threeNumberSum(vector<int> array, int targetSum) {
   // Write your code here.
-  return {};
+  // O(n^2) [for + while] + O(nlogn) [sort] --> O(n^2) time
+  // O(n) space
+
+    sort(array.begin(), array.end());
+
+    vector<vector<int>> triplets;
+
+    for (int i = 0; i < array.size() - 2; i++) {
+        int left = i + 1;
+        int right = array.size() - 1;
+
+        while (left < right) {
+            int currentSum = array[i] + array[left] + array[right];
+
+            if (currentSum == targetSum) {
+                triplets.push_back({array[i], array[left], array[right]});
+                left++;
+                right--;
+            } else if (currentSum < targetSum) {
+                left++;
+            } else if (currentSum > targetSum) {
+                right--;
+            }
+        }
+    }
+
+    return triplets;
 }
 
 int main() {
     vector<vector<int>> result = threeNumberSum({12, 3, 1, 2, -6, 5, -8, 6}, 0);
-
-    // cout << result << endl;
 }
