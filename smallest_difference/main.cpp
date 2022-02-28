e19d565e819167a0737722c933bafe1c7b28576b
chbloca <christen.bc@gmail.com>
2022-02-28 18:53:15 +0100

    implement smallest difference

--- smallest_difference/main.cpp
+++ smallest_difference/main.cpp
@@ -10,12 +10,46 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <climits>
 
 using namespace std;
 
 vector<int> smallestDifference(vector<int> arrayOne, vector<int> arrayTwo) {
-  // Write your code here.
-  return {};
+    // Write your code here.
+    // O(n logn + m logm) time
+    // O(1) space
+
+    sort(arrayOne.begin(), arrayOne.end());
+    sort(arrayTwo.begin(), arrayTwo.end());
+
+    int idxOne = 0;
+    int idxTwo = 0;
+    int smallest = INT_MAX;
+    int current = INT_MAX;
+
+    vector<int> smallestPair;
+
+    while (idxOne < arrayOne.size() && idxTwo < arrayTwo.size()) {
+        int firstNum = arrayOne[idxOne];
+        int secondNum = arrayTwo[idxTwo];
+
+        if (firstNum < secondNum) {
+            current = secondNum - firstNum;
+            idxOne++;
+        } else if (secondNum < firstNum) {
+            current = firstNum - secondNum;
+            idxTwo++;
+        } else { // if they are equal
+            return {firstNum, secondNum};
+        }
+
+        if (smallest > current) {
+            smallest = current;
+            smallestPair = {firstNum, secondNum};
+        }
+    }
+
+    return smallestPair;
 }
 
 int main() {
