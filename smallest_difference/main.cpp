5c1ee032f70dc39414601e1a35d02f7132c10263
chbloca <christen.bc@gmail.com>
2022-02-28 18:29:52 +0100

    add smallest difference implementation

---
+++ smallest_difference/main.cpp
@@ -0,0 +1,23 @@
+/**
+ * Sample Input
+ * arrayOne = [-1, 5, 10, 20, 28, 3]
+ * arrayTwo = [26, 134, 135, 15, 17]
+ * 
+ * Sample Output
+ * [28, 26]
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> smallestDifference(vector<int> arrayOne, vector<int> arrayTwo) {
+  // Write your code here.
+  return {};
+}
+
+int main() {
+    vector<int> result = smallestDifference({-1, 5, 10, 20, 28, 3}, {26, 134, 135, 15, 17});
+}
