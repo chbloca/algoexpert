2955578d1275801855f08ac55bd078c37655792f
chbloca <christen.bc@gmail.com>
2022-02-28 18:57:43 +0100

    add move element to end template

---
+++ move_element_to_end/main.cpp
@@ -0,0 +1,24 @@
+/**
+ * Sample Input
+ * array = [2, 1, 2, 2, 2, 3, 4, 2]
+ * toMove = 2
+ * 
+ * Sample Output
+ * [1, 3, 4, 2, 2, 2, 2, 2] // the numbers 1, 3, and 4 could be ordered differently
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+vector<int> moveElementToEnd(vector<int> array, int toMove) {
+  // Write your code here.
+  return {};
+}
+
+int main() {
+    vector<int> result = moveElementToEnd({2, 1, 2, 2, 2, 3, 4, 2}, 2);
+}
