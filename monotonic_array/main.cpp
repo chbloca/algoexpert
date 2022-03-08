6d49dbab4ef433ad39dc8f7d12ee3a41cd33a020
chbloca <christen.bc@gmail.com>
2022-03-08 19:51:16 +0100

    implement monotonic array

--- monotonic_array/main.cpp
+++ monotonic_array/main.cpp
@@ -12,10 +12,26 @@
 using namespace std;
 
 bool isMonotonic(vector<int> array) {
-  // Write your code here.
-  return false;
+    // Write your code here.
+    // O(n) time
+    // O(1) space
+    bool isNonDecreasing = true;
+    bool isNonIncreasing = true;
+
+    for (int i = 1; i < array.size(); i++) {
+        if (array[i] < array[i - 1]) {
+            isNonDecreasing = false;
+        }
+
+        if (array[i] > array[i - 1]) {
+            isNonIncreasing = false;
+        }
+    }
+
+    return isNonDecreasing || isNonIncreasing;
 }
 
 int main() {
     bool result = isMonotonic({-1, -5, -10, -1100, -1100, -1101, -1102, -9001});
+    cout << result << endl;
 }
