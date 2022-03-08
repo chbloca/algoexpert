af753176896a2f96b7b0c141a391368cd73470af
chbloca <christen.bc@gmail.com>
2022-03-08 19:09:19 +0100

    add monotonic array template

---
+++ monotonic_array/main.cpp
@@ -0,0 +1,21 @@
+/**
+ * Sample Input
+ * array  = [-1, -5, -10, -1100, -1100, -1101, -1102, -9001]
+ * 
+ * Sample Output
+ * true
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool isMonotonic(vector<int> array) {
+  // Write your code here.
+  return false;
+}
+
+int main() {
+    bool result = isMonotonic({-1, -5, -10, -1100, -1100, -1101, -1102, -9001});
+}
