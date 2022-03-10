6558093ef4d4f0a2d06d09235c28d1a1d034c8ee
chbloca <christen.bc@gmail.com>
2022-03-11 00:21:38 +0100

    implement longest peak

--- longest_peak/main.cpp
+++ longest_peak/main.cpp
@@ -12,10 +12,38 @@
 using namespace std;
 
 int longestPeak(vector<int> array) {
-  // Write your code here.
-  return -1;
+    // Write your code here.
+    // O(n) time
+    // O(1) space
+
+    int longestPeakLength = 0;
+
+    int i = 1;
+
+    while (i < array.size() - 1) {
+        bool isPeak = array[i - 1] < array[i] && array[i] > array[i + 1];
+        if (!isPeak) {
+            i++;
+            continue;
+        }
+
+        int leftIdx = i - 2;
+        while (leftIdx >= 0 && array[leftIdx] < array[leftIdx + 1])
+             leftIdx--;
+
+        int rightIdx = i + 2;
+        while (rightIdx < array.size() && array[rightIdx] < array[rightIdx - 1])
+            rightIdx++;
+
+        int currentPeakLength = rightIdx - leftIdx - 1;
+        longestPeakLength = max(longestPeakLength, currentPeakLength);
+        i = rightIdx; 
+    }
+
+    return longestPeakLength;
 }
 
 int main() {
     int result = longestPeak({{1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3}});
+    cout << result << endl;
 }
