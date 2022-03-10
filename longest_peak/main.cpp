378bbbc83bb44ea21ad4ca426855f3f93616d18a
chbloca <christen.bc@gmail.com>
2022-03-10 22:42:14 +0100

    add longest peak

---
+++ longest_peak/main.cpp
@@ -0,0 +1,21 @@
+/**
+ * Sample Input
+ * array = [1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]
+ * 
+ * Sample Output
+ * 6 // 0, 10, 6, 5, -1, -3
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int longestPeak(vector<int> array) {
+  // Write your code here.
+  return -1;
+}
+
+int main() {
+    int result = longestPeak({{1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3}});
+}
