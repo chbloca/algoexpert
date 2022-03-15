c3c98946b20753e9ab1d74f9c9e514b2e217331e
chbloca <christen.bc@gmail.com>
2022-03-15 19:40:41 +0100

    add array of products template

---
+++ array_products/main.cpp
@@ -0,0 +1,26 @@
+/**
+ * Sample Input
+ * array = [5, 1, 4, 2]
+ * 
+ * Sample Output
+ * [8, 40, 10, 20]
+ * 8 is equal to 1 x 4 x 2
+ * 40 is equal to 5 x 4 x 2
+ * 10 is equal to 5 x 1 x 2
+ * 20 is equal to 5 x 1 x 4
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> arrayOfProducts(vector<int> array) {
+  // Write your code here.
+  return {};
+}
+
+
+int main () {
+    vector<int> result = arrayOfProducts({5, 1, 4, 2});
+}
